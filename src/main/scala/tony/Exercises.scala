c91037fc33f7ff6ff7079f25142d6e86871cf439
Josh Cough <joshcough@gmail.com>
2010-03-03 23:26:08 -0600

    started

---
+++ src/main/scala/tony/Exercises.scala
@@ -0,0 +1,11 @@
+package tony
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: HP_Administrator
+ * Date: Feb 25, 2010
+ * Time: 9:38:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Exercises
---
+++ src/main/scala/tony/Intermediate.scala
@@ -0,0 +1,11 @@
+package tony
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: HP_Administrator
+ * Date: Feb 25, 2010
+ * Time: 11:30:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Intermediate
