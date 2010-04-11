3974089700e4c752822867c0bccdf23e0b5a448b
Josh Cough <joshcough@gmail.com>
2010-04-11 11:53:26 -0500

    parsing instructions appears to be working

---
+++ src/main/scala/L1Compiler/L1AST.scala
@@ -0,0 +1,60 @@
+package L1Compiler
+
+object L1AST {
+
+  trait L1
+
+  trait Instruction
+  trait S extends Instruction
+  case class Num(n: Int) extends S
+  case class Label(l: String) extends S
+  abstract class Register(name: Symbol) extends S
+  object XRegister {
+    def apply(s: Symbol): Option[XRegister] = s match {
+      case 'esi => Some(esi)
+      case 'edi => Some(edi)
+      case 'ebp => Some(ebp)
+      case 'esp => Some(esp)
+      case _ => None
+    }
+  }
+  sealed abstract case class XRegister(x: Symbol) extends Register(x)
+  object esi extends XRegister('esi)
+  object edi extends XRegister('edi)
+  object ebp extends XRegister('ebp)
+  object esp extends XRegister('esp)
+  object CXRegister {
+    def apply(s: Symbol): Option[CXRegister] = s match {
+      case 'eax => Some(eax)
+      case 'ecx => Some(ecx)
+      case 'edx => Some(edx)
+      case 'ebx => Some(ebx)
+      case _ => None
+    }
+  }
+  sealed abstract case class CXRegister(cx: Symbol) extends Register(cx)
+  object eax extends CXRegister('eax)
+  object ecx extends CXRegister('ecx)
+  object edx extends CXRegister('edx)
+  object ebx extends CXRegister('ebx)
+
+  case class Comp(s1: S, op: CompOp, s2: S) extends Instruction
+  sealed abstract case class CompOp(op: String)
+  object LessThan extends CompOp("<")
+  object LessThanOrEqualTo extends CompOp("<=")
+  object EqualTo extends CompOp("=")
+  case class RegisterAssignment(r: Register, s: Instruction) extends Instruction
+  case class MemLoc(basePointer: Register, offset: Num) extends Instruction
+  case class MemRead(loc: MemLoc) extends Instruction
+  case class MemWrite(loc: MemLoc, e: S) extends Instruction
+  case class Print(e: S) extends Instruction
+  case class Allocate(r: Register, init: Num) extends Instruction
+  case class Increment(x: Register, s: S) extends Instruction
+  case class Decrement(x: Register, s: S) extends Instruction
+  case class Multiply(x: Register, s: S) extends Instruction
+  // TODO: check if interpreter allows (goto num) and (goto register)
+  case class Goto(s: S) extends Instruction
+  case class CJump(comp:Comp, l1: Label, l2: Label) extends Instruction
+  case class Call(s:S) extends Instruction
+  case object Return extends Instruction
+}
--- src/main/scala/L1Compiler/L1Parser.scala
+++ src/main/scala/L1Compiler/L1Parser.scala
@@ -1,100 +1,84 @@
 package L1Compiler
 
 import reader._
+import L1AST._
 
-/**
-| (x += s)            ;; increment x by s
-| (x -= s)            ;; decrement x by s
-| (x *= s)            ;; multiply x by x
-| (cx <- s cmp s)     ;; save result of a comparison
-| (goto s)            ;; unconditional jump
-| (cjump s cmp s label label) ;; conditional jump
-| (call s)            ;; call a function [see below]
-| (return)            ;; return from a function
+object L1Parser extends Parser[L1] {
 
-And a few helper non-terminals:
-
-s     ::= x | num | label
-x     ::= cx | esi | edi | ebp | esp
-cx    ::= eax | ecx | edx | ebx
-cmp   ::= < | <= | =
-
-num   ::= number between (inclusive) -2^31 and (2^31)-1
- **/
-trait Exp
-case class Num(n: Int) extends Exp
-case class Label(l: String) extends Exp
-abstract class Register(name: Symbol) extends Exp
-object XRegister {
-  def apply(s: Symbol): Option[XRegister] = s match {
-    case 'esi => Some(esi)
-    case 'edi => Some(edi)
-    case 'ebp => Some(ebp)
-    case 'esp => Some(esp)
-    case _ => None
-  }
-}
-sealed abstract case class XRegister(x: Symbol) extends Register(x)
-object esi extends XRegister('esi)
-object edi extends XRegister('edi)
-object ebp extends XRegister('ebp)
-object esp extends XRegister('esp)
-object CXRegister {
-  def apply(s: Symbol): Option[CXRegister] = s match {
-    case 'eax => Some(eax)
-    case 'ecx => Some(ecx)
-    case 'edx => Some(edx)
-    case 'ebx => Some(ebx)
-    case _ => None
-  }
-}
-sealed abstract case class CXRegister(cx: Symbol) extends Register(cx)
-object eax extends CXRegister('eax)
-object ecx extends CXRegister('ecx)
-object edx extends CXRegister('edx)
-object ebx extends CXRegister('ebx)
-sealed abstract case class Comp(op: String) extends Exp
-object LessThan extends Comp("<")
-object LessThanOrEqualTo extends Comp("<=")
-object EqualTo extends Comp("=")
-
-case class RegisterAssignment(r: Register, s: Exp) extends Exp
-case class MemLoc(basePointer: Register, offset: Num) extends Exp
-case class MemRead(loc: MemLoc) extends Exp
-case class MemWrite(loc: MemLoc, e:Exp) extends Exp
-
-case class Print(e:Exp) extends Exp
-case class Allocate(r:Register, init:Num) extends Exp
-
-object L1Parser extends Parser[Exp] {
-  def parse(expr: Any): Exp = {
-    expr match {
-      case s: Symbol => parseLabelOrRegister(s)
-      case n: Int => Num(n)
-      case List(x: Any, '<-, s: Any) => parseAssignment(x, s)
-      case _ => error("unexpected token: " + expr)
-    }
+  def parse(exp:Any): L1 = {
+    new L1{}
   }
 
-  def parseLabelOrRegister(s: Symbol): Exp = {
-    val chop = s.toString.drop(1) // remove the ' from ':label
-    if (chop.startsWith(":")) parseLabel(chop) else parseRegister(s)
+  def parseInstruction(expr: Any): Instruction = expr match {
+    // TODO: had to put this here because of a serious compiler bug. investigate.
+    // (cjump s cmp s label label) ;; conditional jump
+    case 'cjump :: xs => parseCJump(expr)
+    case s: Symbol => parseLabelOrRegister(s)
+    // num   ::= number between (inclusive) -2^31 and (2^31)-1
+    // TODO: check range
+    case n: Int => Num(n)
+    case List(x: Any, '<-, s: Any) => parseAssignment(x, s)
+    // TODO: maybe just call this directly instead of going through parseAssignment
+    case List(x: Any, '<-, s1: Any, cmp: Symbol, s2:Any) => parseAssignment(x, (s1, cmp, s2))
+    // math ops
+    case List(s1: Symbol, '+=, s2:Any) => Increment(parseRegister(s1), parseNumOrRegister(s2))
+    case List(s1: Symbol, '-=, s2:Any) => Decrement(parseRegister(s1), parseNumOrRegister(s2))
+    case List(s1: Symbol, '*=, s2:Any) => Multiply(parseRegister(s1), parseNumOrRegister(s2))
+    case List('goto, s:Any) => Goto(parseS(s))
+    case List('call, s:Any) => Call(parseS(s))
+    case List('return) => Return
+    case _ => error("unexpected token: " + expr)
+  }
+
+  def parseS(exp:Any) = exp match {
+    case n: Int => Num(n)
+    case s: Symbol => parseLabelOrRegister(s)
+  }
+
+  def parseNumOrRegister(exp:Any): S = exp match {
+    case n: Int => Num(n)
+    case s: Symbol => parseRegister(s)
+  }
+
+  def parseLabelOrRegister(s: Symbol): S = {
+    if (s.toString.startsWith("':")) parseLabel(s.toString) else parseRegister(s)
   }
 
   // label ::= sequence of alpha-numeric characters or underscore,
   // but starting with a colon, ie matching this regexp:
   // #rx"^:[a-zA-Z0-9_]$"
   def parseLabel(s: String) = {
+    val chop = s.toString.drop(1) // remove the ' from ':label
     // TODO: put some legit error checking here...
-    Label(s.drop(1)) // remove the : from :label.
+    Label(s.drop(2)) // remove the ' and : from ':label.
   }
 
   def parseRegister(s: Symbol): Register = {
-    XRegister(s).getOrElse(CXRegister(s).getOrElse(error(s + " is an invalid register")))
+    XRegister(s).getOrElse(CXRegister(s).getOrElse(error(s.toString.drop(1) + " is an invalid register")))
+  }
+
+  def parseCxRegister(s: Symbol): CXRegister = {
+    CXRegister(s).getOrElse(error(s.toString.drop(1) + " is an invalid cx register"))
+  }
+
+  def parseComp(s1: Any, cmp: Symbol, s2: Any): Comp = {
+    Comp(parseNumOrRegister(s1), parseCompOp(cmp),parseNumOrRegister(s2))
+  }
+
+  def parseCompOp(s:Symbol): CompOp = s match {
+    case '< => LessThan
+    case '<= => LessThanOrEqualTo
+    case '= => EqualTo
+    case _ => error(s + " is an invalid comparison operator")
+  }
+
+  def parseCJump(exp: Any) = exp match {
+    case List('cjump, s1, cmp: Symbol, s2, l1: Symbol, l2: Symbol) =>
+      CJump(parseComp(s1, cmp, s2), parseLabel(l1.toString), parseLabel(l2.toString))
+    case _ => error("bad cjump: " + exp)
   }
 
   def parseAssignment(x: Any, s: Any) = {
-    //println("parsing assignment of: " + x + " to: " + s)
     (x, '<-, s) match {
       //(x <- s) ;; assign to a register
       case (x: Symbol, '<-, i: Int) => RegisterAssignment(parseRegister(x), Num(i))
@@ -103,18 +87,18 @@ object L1Parser extends Parser[Exp] {
       case (x1: Symbol, '<-, List('mem, x2:Symbol, n4:Int)) =>
         RegisterAssignment(parseRegister(x1), MemRead(MemLoc(parseRegister(x2), Num(n4))))
       // | ((mem x n4) <- s)   ;; update memory @ x+n4
-      case (List('mem, x:Symbol, n4:Int), '<-, i: Int) =>
-        MemWrite(MemLoc(parseRegister(x), Num(n4)), Num(i))
-      case (List('mem, x:Symbol, n4:Int), '<-, s: Symbol) =>
-        MemWrite(MemLoc(parseRegister(x), Num(n4)), parseRegister(s))
+      case (List('mem, x:Symbol, n4:Int), '<-, x2) =>
+        MemWrite(MemLoc(parseRegister(x), Num(n4)), parseNumOrRegister(x2))
       // ;; two calls into the runtime system, one to print a value:
       //(eax <- (print s))
-      // TODO: figure out if this is even legal (print 8)      
-      case ('eax, '<-, List('print, i:Int)) => Print(Num(i))
-      case ('eax, '<-, List('print, s:Symbol)) => Print(parseRegister(s))
+      // TODO: figure out if this is even legal (print 8)
+      case ('eax, '<-, List('print, s)) => Print(parseNumOrRegister(s))
       // ;; and one to allocate & initialize some space
       // (eax <- (allocate s s))
       case ('eax, '<-, List('allocate, s:Symbol, i:Int)) => Allocate(parseRegister(s), Num(i))
+      //(cx <- s cmp s)
+      case (cx:Symbol, '<-, (s1: Any, cmp: Symbol, s2: Any)) =>
+        RegisterAssignment(parseCxRegister(cx), parseComp(s1, cmp, s2))
       case _ => error("bad assignment statement")
     }
   }
