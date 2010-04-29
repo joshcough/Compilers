fabf3f9e7d24e6f74850a3fb9a154ade97345654
josh <joshcough@ubuntu.ubuntu-domain>
2010-04-29 09:52:22 -0500

    wrapping up

---
+++ src/main/scala/L2Compiler/HW2Creator.scala
@@ -0,0 +1,27 @@
+package L2Compiler
+
+import L1Compiler.FileHelper._
+import java.io.File
+
+object HW2Creator {
+  def main(args:Array[String]){
+    var count = 0
+    for(test <- new File("./spilltests.txt").read.split("\n")){
+      new File("./test/test" + count + ".L2f").write(test + " x -4 s_")
+      count += 1
+    }
+  }
+}
+
+import L1Compiler.CommandRunner._
+
+object HW2TestResultsCreator{
+  def main(args:Array[String]){
+    for(f <- new File("./test").listFiles; if(f.getName.endsWith(".L2f"))){
+      val livenessResFile = "./test/" + f.getName.dropRight(3) + "lres"
+      val spillResFile = "./test/" + f.getName.dropRight(3) + "sres"
+      new File(spillResFile).write(runAndDieOneErrors("./spill " + "./test/" + f.getName))
+      new File(livenessResFile).write(runAndDieOneErrors("./liveness " + "./test/" + f.getName))
+    }
+  }  
+}
