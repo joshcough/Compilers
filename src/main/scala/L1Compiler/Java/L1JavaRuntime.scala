624691d3561014905143d3066bd28a328ce37e3d
joshcough <joshcough@gmail.com>
2011-04-01 00:24:34 -0500

    lots of features added to the runtime, and tests for them.

--- src/main/scala/L1Compiler/Java/L1JavaRuntime.scala
+++ src/main/scala/L1Compiler/Java/L1JavaRuntime.scala
@@ -1,50 +1,20 @@
 package L1Compiler.Java
 
-class L1JavaRuntime {
-  /**
-   *      ::= x | num | label
-x     ::= cx | esi | edi | ebp | esp
-cx    ::= eax | ecx | edx | ebx
-sx    ::= ecs
-aop=  ::= += | -= | *= | &=
-sop=  ::= <<= | >>=
-cmp   ::= < | <= | =
-label ::= sequence of alpha-numeric characters or underscore,
-          but starting with a colon, ie matching this regexp:
-          #rx"^:[a-zA-Z0-9_]$"
-num   ::= number between (inclusive) -2^31 and (2^31)-1
-n4    ::= number between (inclusive) -2^31 and (2^31)-1
-          that is divisble by 4
+object L1JavaRuntime {
 
-  ;; ebx, esi, and edi are callee / function save
-  ;; eax, edx, and ecx are caller / application save / arguments (in that order)
-
-   */
-
-  var eax, ebx, ecx, edx, esi, ebp, esp = 0
+  // this allows us to put labels into them and other stuff...
+  // maybe.
+  case class Register(var value:Any = new AnyRef){
+    def clear(){ value = new AnyRef }
+  }
+  var eax, ebx, ecx, edx, esi, ebp, esp = new Register
+  val registers = List(eax, ebx, ecx, edx, esi, ebp, esp)
 
   val HEAP_SIZE = 1048576  // one megabyte
-  val heap = new Array[Int](HEAP_SIZE)
-
-  def print(l: Int): String = printContent(l) + "\n"
-
-  def printContent(in: Int, depth: Int=0): String = {
-    if (depth >= 4) "..."
-    else if ((in & 1) == 1) (in >> 1).toString
-    else {
-      val ptr = (in >> 1)
-      val size = heap(ptr)
-      "{s:" + size + ", " +
-      (for (data <- (ptr + 1) until (ptr + 1 + size)) yield
-        printContent(heap(data), depth + 1)).mkString(", ") +
-      "}"
-    }
-  }
-
-  def heapView = heap.take(wordsAllocated).toList
-
+  val heap = new Array[Any](HEAP_SIZE)
   var allocptr = 0
   var wordsAllocated = 0
+  def heapView = heap.take(wordsAllocated).toList
 
   def allocate(fw_size: Int, fw_fill: Int): Int = {
     val size = fw_size >> 1
@@ -65,9 +35,71 @@ n4    ::= number between (inclusive) -2^31 and (2^31)-1
     }
   }
 
-//int main() {
-//  go();   // call into the generated code
+  def print(a: Any): String = a match {
+    case i:Int => printContent(i) + "\n"
+    case Register(v) => print(v)
+    case _ => error("don't know how to print: " + a)
+  }
 
+  def printContent(in: Int, depth: Int=0): String = {
+    if (depth >= 4) "..."
+    else if ((in & 1) == 1) (in >> 1).toString
+    else {
+      val ptr = (in >> 1)
+      val size = heap(ptr).asInstanceOf[Int]
+      "{s:" + size + ", " +
+      (for (data <- (ptr + 1) until (ptr + 1 + size)) yield heap(data) match {
+        case i: Int => printContent(i, depth + 1)
+        case a => error("don't know how to print: " + a)
+      }).mkString(", ") + "}"
+    }
+  }
+
+  // for testing
+  def newProgram(){
+    allocptr = 0
+    wordsAllocated = 0
+    for(i <- 0 until HEAP_SIZE) heap(i) = 0
+    registers.foreach(_.clear())
+  }
+
+  /**
+   * i ::= (x <- s)            ;; assign to a register
+    | (x <- (mem x n4))   ;; read from memory @ x+n4
+    | ((mem x n4) <- s)   ;; update memory @ x+n4
+   */
+  def mov(dest:Register, value:Any) = value match {
+    case Register(v) => dest.value = v
+    case _ => dest.value = value.asInstanceOf[AnyRef]
+  }
+
+  //(mem x n4))   ;; read from memory @ x+n4
+  def read(x:Register, n4:Int): Any = x.value match {
+    case i if i.isInstanceOf[Int] => heap(i.asInstanceOf[Int] + n4 / 4)
+    case bad => error("cant read from bad memory address: " + bad)
+  }
+
+  //((mem x n4) <- s)   ;; update memory @ x+n4
+  def write(x:Register, n4:Int, s:Any): Unit = x.value match {
+    case i if i.isInstanceOf[Int] => heap(i.asInstanceOf[Int] + n4 / 4) = s
+    case bad => error("cant write to bad memory address: " + bad)
+  }
+
+  //(x aop= s)
+  // s ::= x | num | label
+  //aop=  ::= += | -= | *= | &=
+  def +=(x:Register, s:Any) { aop(x, s)("add", _ + _) }
+  def -=(x:Register, s:Any) { aop(x, s)("minus", _ - _) }
+  def *=(x:Register, s:Any) { aop(x, s)("times", _ * _) }
+  def &=(x:Register, s:Any) { aop(x, s)("and", _ & _) }
+
+  def aop(x:Register, s:Any)(name:String, f: (Int, Int) => Int) {
+    s match {
+      case i: Int => x.value match {
+        case v if v.isInstanceOf[Int] => x.value = f(v.asInstanceOf[Int], i).asInstanceOf[AnyRef]
+        case bad => error("bad contents in register for '" + name + "' operation. contents: " + bad)
+      }
+      case _ => error("can't add this to a register: " + s)
+    }
+  }
 }
-
-
--- src/test/scala/L1Compiler/Java/L1JavaRuntimeTests.scala
+++ src/test/scala/L1Compiler/Java/L1JavaRuntimeTests.scala
@@ -4,35 +4,63 @@ import org.scalatest.FunSuite
 
 class L1JavaRuntimeTests extends FunSuite {
 
+  import L1JavaRuntime._
+
   testPrint(5, 2)
   testPrint(3, 1)
   testPrint(1, 0)
   testPrint(-1, -1)
   testPrint(-3, -2)
 
-  def testPrint(i:Int, expected: Int) = {
-    test("print " + (i -> expected)){
-      assert(new L1JavaRuntime().print(i).trim === expected.toString)
-    }
+  def runtimeTest(name:String)(f: => Unit) = test(name){ newProgram(); f }
+  def testPrint(i:Int, expected: Int) = runtimeTest("print " + (i -> expected)){
+    assert(valueOf(i) === expected.toString)
   }
 
-  test("alloc"){
-    val runtime = new L1JavaRuntime()
+  runtimeTest("alloc"){
     // allocate a size 10 array filled with 5's
     // 21 is 10 endcoded
-    runtime.allocate(21, 5)
-    assert(runtime.heapView === List(10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5))
-    assert(runtime.print(0).trim === "{s:10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}")
+    allocate(21, 5)
+    assert(heapView === List(10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5))
+    assert(valueOf(0) === "{s:10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}")
   }
 
-  test("alloc twice"){
-    val runtime = new L1JavaRuntime()
+  runtimeTest("alloc twice"){
     // allocate a size 10 array filled with 5's
     // 21 is 10 endcoded
-    assert(runtime.allocate(21, 5) === 0)
-    assert(runtime.allocate(21, 7) === 11)
-    assert(runtime.print(0).trim === "{s:10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}")
-    assert(runtime.heapView === List(10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7))
-    assert(runtime.print(22).trim === "{s:10, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}")
+    assert(allocate(21, 5) === 0)
+    assert(allocate(21, 7) === 11)
+    assert(valueOf(0) === "{s:10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}")
+    assert(heapView === List(10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7))
+    assert(valueOf(22) === "{s:10, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}")
   }
+
+  runtimeTest("registers"){
+    mov(eax, 85)
+    mov(ebx, ":rwer")
+    mov(ebx, eax)
+    assert(valueOf(eax) === "42")
+    assert(valueOf(ebx) === "42")
+  }
+
+  runtimeTest("read"){
+    mov(eax, allocate(21, 5))
+    mov(eax, allocate(21, 7))
+    val size = read(eax, 0).asInstanceOf[Int]
+    for(i <- 1 until size) assert(valueOf(read(eax, i * 4)) === "3")
+  }
+
+  runtimeTest("+="){
+    mov(eax, 7)
+    +=(eax, 2)
+    assert(valueOf(eax) === "4")
+    -=(eax, 4)
+    assert(valueOf(eax) === "2")
+    *=(eax, 3)
+    assert(valueOf(eax) === "7")
+    // TODO test &=
+  }
+
+  def valueOf(a:Any) = print(a).trim
+
 }
