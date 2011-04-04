1df8fd0a742dd757b275d359a5b4fdd326056524
josh <joshcough@ubuntu.ubuntu-domain>
2011-04-04 09:41:44 -0500

    moved a few things around. nbd.

--- bin/hw-script.sh
+++ bin/hw-script.sh
@@ -1,14 +1,15 @@
 cd ..
-rm -rf Compilers
-cp -r Interpreters Compilers
-cd Compilers
+rm -rf compilers-hw1
+cp -r Interpreters compilers-hw1
+cd compilers-hw1
 sbt package
 cp target/scala_2.8.1/compilers_2.8.1-0.1.jar ./lib/compiler.jar
 cp project/boot/scala-2.8.1/lib/scala-library.jar ./lib
 rm -rf .git project target out lib_managed 444-code src/main/scheme Test.class Test.j compilers.iml runtime.o test.S	test2.S
 # just for now:
 rm liveness spill spill-tests.txt  spilltests.txt 
-tar cfvz Compilers.tar.gz *
-mv Compilers.tar.gz ..
+mv bin/L1 .
+tar cfvz compilers-hw1.tar.gz *
+mv compilers-hw1.tar.gz ..
 echo "done. now run:"
-echo "scp ./Compilers.tar.gz joshcough@partsims.ccl.northwestern.edu:./"
+echo "scp ./compilers-hw1.tar.gz jdc974@some.machine.edu:./"
