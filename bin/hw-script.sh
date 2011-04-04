7ff1ec9c494096e9699e589786ceb7675aa52b99
josh <joshcough@ubuntu.ubuntu-domain>
2011-04-04 10:09:07 -0500

    random crap

--- bin/hw-script.sh
+++ bin/hw-script.sh
@@ -5,11 +5,11 @@ cd compilers-hw1
 sbt package
 cp target/scala_2.8.1/compilers_2.8.1-0.1.jar ./lib/compiler.jar
 cp project/boot/scala-2.8.1/lib/scala-library.jar ./lib
-rm -rf .git project target out lib_managed 444-code src/main/scheme Test.class Test.j compilers.iml runtime.o test.S	test2.S
+rm -rf .git project target out lib_managed 444-code src/main/scheme Test.class Test.j compilers.iml runtime.o test.S test2.S a.out bin Test.o Test.S
 # just for now:
 rm liveness spill spill-tests.txt  spilltests.txt 
 mv bin/L1 .
 tar cfvz compilers-hw1.tar.gz *
 mv compilers-hw1.tar.gz ..
 echo "done. now run:"
-echo "scp ./compilers-hw1.tar.gz jdc974@some.machine.edu:./"
+echo "scp ./compilers-hw1.tar.gz jdc974@tlab-18.cs.northwestern.edu:./spring2011/"
