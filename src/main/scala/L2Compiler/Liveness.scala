d8adce90cb69d57fb493dd2add42eae7f53d6c46
joshcough <joshcough@gmail.com>
2011-04-05 00:28:28 -0500

    liveness now compiling again

--- src/main/scala/L2Compiler/Liveness.scala
+++ src/main/scala/L2Compiler/Liveness.scala
@@ -1,75 +1,88 @@
 package L2Compiler
-/*
+
 import L2AST._
 import L1Compiler.L1AST._
 
-case class LiveRange(x:X, range:Int)
+case class LiveRange(register:Register, range:Int)
 
 // ebx, esi, and edi are callee / function save
 // eax, edx, and ecx are caller / application save / arguments (in that order)
 trait Liveness {
 
-  def gen(i:Instruction): Set[X] = {
-    def g(i: Instruction, s2: S) = gen(i) union gen(s2)
-    i match {
-      case Num(_) => Set()
-      case Label(_) => Set()
-      case x:X => Set(x)
-      case Allocate(n, init) => g(n,init)
-      case Assignment(x, i) => gen(i)
-      case Increment(x, s) => g(x, s)
-      case Decrement(x, s) => g(x, s)
-      case Multiply(x, s) => g(x, s)
-      case LeftShift(x, s) => g(x, s)
-      case RightShift(x, s) => g(x, s)
-      case BitwiseAnd(x, s) => g(x, s)
-      case Comp(s1, op, s2) => g(s1,s2)
-      case CJump(comp, l1, l2) => gen(comp)
-      case MemLoc(bp, _) => Set(bp)
-      case MemRead(loc) => gen(loc)
-      case MemWrite(loc, s) => g(loc,s)
+  def gen(i:Instruction): Set[Register] = {
+    def gen(rhs:AssignmentRHS): Set[Register] = rhs match {
+      case r:Register => Set(r)
       case Print(s) => gen(s)
+      case Allocate(n, init) => gen(n)  union gen(init)
+      case ArrayError(a, n)  => gen(a)  union gen(n)
+      case Comp(s1, op, s2)  => gen(s1) union gen(s1)
+      case MemRead(MemLoc(bp, _)) => Set(bp)
+      case n:Num => Set()
+      case l:Label => Set()
+    }
+    i match {
+      case Assignment(x, i) => gen(i)
+      case Increment(x, s)  => gen(x) union gen(s)
+      case Decrement(x, s)  => gen(x) union gen(s)
+      case Multiply(x, s)   => gen(x) union gen(s)
+      case LeftShift(x, s)  => gen(x) union gen(s)
+      case RightShift(x, s) => gen(x) union gen(s)
+      case BitwiseAnd(x, s) => gen(x) union gen(s)
+      case CJump(comp, l1, l2) => gen(comp)
+      case MemWrite(MemLoc(bp, _), s) => Set(bp) union gen(s)
       case Goto(s) => gen(s)
       case Call(s) => gen(s)
+      case TailCall(s) => gen(s) // this is an educated guess
       case Return => Set(ebx, edi, esi)
-      case _ => Set()
+      case LabelDeclaration(_) => Set()
     }
   }
-  def kill(i:Instruction): Set[X] = i match {
-    case Allocate(n, init) => Set(eax)
-    case Assignment(x, _) => Set(x)
-    case Increment(x, _) => Set(x)
-    case Decrement(x, _) => Set(x)
-    case Multiply(x, _) => Set(x)
-    case LeftShift(x, _) => Set(x)
-    case RightShift(x, _) => Set(x)
-    case BitwiseAnd(x, _) => Set(x)
-    case MemLoc(bp, _) => Set()
-    case MemRead(loc) => Set()
-    case MemWrite(loc, s:X) => Set()
-    case Print(s) => Set(eax)
-    case Goto(s) => Set()
-    case Comp(s1, op, s2) => Set()
-    case CJump(comp, l1, l2) => Set()
-    case Call(s) => Set(eax, edx, ecx)
-    case Return => Set()
-    case _ => Set()
+  
+  def kill(i:Instruction): Set[Register] = {
+    def kill(rhs:AssignmentRHS): Set[Register] = rhs match {
+      case r:Register => Set()
+      case Print(s) => Set(eax) // hmmm...i thought others were killed here as well.
+      case Allocate(n, init) => Set(eax) // same here
+      case ArrayError(a, n)  => Set(eax) // same here
+      case Comp(s1, op, s2)  => Set()
+      case MemRead(MemLoc(bp, _)) => Set()
+      case n:Num => Set()
+      case l:Label => Set()
+    }
+    i match {
+      case Assignment(x, _) => Set(x)
+      case Increment(x, _) => Set(x)
+      case Decrement(x, _) => Set(x)
+      case Multiply(x, _) => Set(x)
+      case LeftShift(x, _) => Set(x)
+      case RightShift(x, _) => Set(x)
+      case BitwiseAnd(x, _) => Set(x)
+      case MemWrite(_, _) => Set()
+      case Goto(s) => Set()
+      case CJump(comp, l1, l2) => Set()
+      case Call(s) => Set(eax, edx, ecx)
+      case TailCall(s) => Set(eax, edx, ecx) // this is an educated guess
+      case Return => Set()
+      case LabelDeclaration(_) => Set()
+    }
   }
 
+
+
   /**
-      in(n) = gen(n-th-inst) ∪ (out (n) - kill(n-th-inst))
-      out(n) = ∪{in(m) | m ∈ succ(n)}
-    */
-  case class InstuctionInOutSet(i:Instruction, in:Set[X], out:Set[X])
+   * in(n) = gen(n-th-inst) ∪ (out (n) - kill(n-th-inst))
+   * out(n) = ∪{in(m) | m ∈ succ(n)}
+   */
+  case class InstuctionInOutSet(i:Instruction, in:Set[Register], out:Set[Register])
   def inout(f:Func): List[InstuctionInOutSet] = {
-    val (head::rest) = inout((f.name :: f.body).map(InstuctionInOutSet(_, Set[X](), Set[X]())))
+    val (head::rest) = inout((f.name :: f.body).map(InstuctionInOutSet(_, Set[Register](), Set[Register]())))
     val newHead = head.copy(in = head.in - ebx - edi - esi)
     newHead :: rest
   }
 
   private def inout(acc:List[InstuctionInOutSet]): List[InstuctionInOutSet] = {
     def in(iios:InstuctionInOutSet) = gen(iios.i) union (iios.out -- kill(iios.i))
-    val next = acc.foldRight((List[InstuctionInOutSet](), Set[X]())){
+    val next = acc.foldRight((List[InstuctionInOutSet](), Set[Register]())){
       case (iios, (acc, lastIn)) => {
         val newIn = in(iios)
         (InstuctionInOutSet(iios.i, newIn, lastIn) :: acc, newIn)
@@ -82,7 +95,7 @@ trait Liveness {
    * TODO - not yet using the kill set as part of interference!
    * TODO cx <- instructions
    */
-  def buildInterferenceSet(iioss: List[InstuctionInOutSet]): Set[(X,X)] = {
+  def buildInterferenceSet(iioss: List[InstuctionInOutSet]): Set[(Register,Register)] = {
     val ins = iioss.map(_.in)
     val outs = iioss.map(_.out)
     val in_interference = ins.flatMap{ s => for(x <- s; y <- s; if(x!=y)) yield (x,y) }.toSet
@@ -91,7 +104,7 @@ trait Liveness {
   }
 
   def liveRanges(iioss: List[InstuctionInOutSet]): List[List[LiveRange]] = {
-    def liveRanges(x: X, sets: List[List[X]]): List[LiveRange] = sets match {
+    def liveRanges(x: Register, sets: List[List[Register]]): List[LiveRange] = sets match {
       case Nil => Nil
       case y::ys => {
         if(y contains x) {
@@ -101,8 +114,7 @@ trait Liveness {
       }
     }
     val inSets = iioss.map(_.in)
-    val variablesAndRegisters = inSets.foldLeft(Set[X]()){ case (acc, s) => acc union s}
+    val variablesAndRegisters = inSets.foldLeft(Set[Register]()){ case (acc, s) => acc union s}
     for(x <- variablesAndRegisters.toList) yield liveRanges(x, inSets.map(_.toList))
   }
 }
-*/
