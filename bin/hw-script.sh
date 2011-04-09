3719e10354744f84b6bc3af6d2e64e4674dc0324
joshcough <joshcough@gmail.com>
2011-04-09 17:20:16 -0500

    update build script for building everything instead of just hw1

--- bin/hw-script.sh
+++ bin/hw-script.sh
@@ -8,20 +8,24 @@ git clone git@github.com:joshcough/Interpreters.git
 cd Interpreters
 
 #build the code
+sbt update
+sbt test
 sbt package
-#sbt test
 
 # move a few things into place
 cp target/scala_2.8.1/compilers_2.8.1-0.1.jar ./lib/compiler.jar
 cp project/boot/scala-2.8.1/lib/scala-library.jar ./lib
-mv bin/L1 .
+mv bin/L1 bin/spill bin/liveness .
 
 # get rid of stuff i dont need to hand in
 rm -rf .git project target out lib_managed test 444-code src/main/scheme Test.class Test.j compilers.iml runtime.o test.S test2.S a.out bin Test.o Test.S
 
 # create the tar to hand in
-tar cfvz ../compilers-hw1.tar.gz *
+tar cfvz ../compilers.tar.gz *
 
-# done
-echo "done. now run:"
-echo "scp ./build/compilers-hw1.tar.gz jdc974@tlab-18.cs.northwestern.edu:./spring2011/"
+# done creating compilers.tar.gz
+echo "done creating compilers.tar.gz. now run:"
+echo "scp ./build/compilers.tar.gz jdc974@tlab-18.cs.northwestern.edu:./spring2011/"
+
+tar cfvz spill-test.tar.gz spill-test
+echo "done creating ./build/spill-test.tar.gz."
