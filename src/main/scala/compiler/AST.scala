f633ffeedf8142c113775244f193ce021edfefb6
Josh Cough <joshcough@gmail.com>
2011-05-12 15:07:48 -0500

    mucking around, but i dont expect this to go far

---
+++ src/main/scala/compiler/AST.scala
@@ -0,0 +1,11 @@
+package compiler
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jdc974
+ * Date: 5/12/11
+ * Time: 3:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AST
