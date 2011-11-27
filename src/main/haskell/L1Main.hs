6c37e68dd283a7a29208923528226b752567a7d1
Josh Cough <joshcough@gmail.com>
2011-11-27 11:48:21 -0500

    preparing to call the compiler

--- src/main/haskell/L1Main.hs
+++ src/main/haskell/L1Main.hs
@@ -1,1 +1,10 @@
-main = putStrLn "hello"
+import System.Environment 
+import System.IO
+import Data.List
+
+main = do  
+   fileNames <- getArgs
+   -- just read the first file here.
+   -- i suppose later on we could compile many files...
+   contents <- readFile $ fileNames !! 0
+   putStrLn contents
