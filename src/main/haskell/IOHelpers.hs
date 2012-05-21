4b5926ec10a05085d31e9bfee0edb6940030ee47
joshcough <joshcough@gmail.com>
2012-05-21 09:01:09 -0400

    some refactorings

--- src/main/haskell/IOHelpers.hs
+++ src/main/haskell/IOHelpers.hs
@@ -12,20 +12,63 @@ import Control.Applicative
 
 (|>) = flip (.)
 ($$>) = flip fmap
+listFiles = getDirectoryContents
 
-putStringAndNL :: String -> IO ()
-putStringAndNL s = putStrLn s >> putStrLn "\n"
-
-putStringsAndNLs :: [String] -> IO ()
-putStringsAndNLs xs = sequenceA_ (map putStringAndNL xs)
-
+-- put a whole list of showables
 putList :: Show a => [a] -> IO ()
 putList xs = sequenceA_ (map (putStrLn . show) xs)
 
+-- put the given a, and then a new line
+putWithNewLine :: Show a => a -> IO ()
+putWithNewLine a = putStrLn (show a) >> putStrLn "\n"
+
+-- put each a in the given list, and a new line after each too.
+putWithNewLineList :: Show a => [a] -> IO ()
+putWithNewLineList xs = sequenceA_ (map putWithNewLine xs)
+
 -- full pathnames for every file in the given directory
 filesWithFullPaths :: FilePath -> IO [FilePath]
-filesWithFullPaths dir = 
-  drop 2 <$> getDirectoryContents dir $$> (map (\f -> dir ++ "/" ++ f))
+filesWithFullPaths dir = drop 2 <$> listFiles dir $$> (map (\f -> dir ++ "/" ++ f))
+
+--sequenceA_ :: (Foldable t, Applicative f) => t (f a) -> f ()
+putFileNames :: FilePath -> IO ()
+putFileNames dir = filesWithFullPaths dir >>= putList 
+
+-- checks to see if the contents of the given files are equal
+filesEqual :: FilePath -> FilePath -> IO Bool
+filesEqual f1 f2 = (==) <$> (readFile f1) <*> (readFile f2)
+
+-- checks to see if the two given lists
+--  1) contain all the same filenames
+--  2) the contents of the files are equal
+fileListEqual :: [FilePath] -> [FilePath] -> IO Bool
+fileListEqual fs1 fs2 = (sort fs1 == sort fs2 &&) <$> contentsEqual where
+  contentsEqual :: IO Bool
+  contentsEqual =
+    Data.List.foldl (\b (f1, f2) -> (&&) <$> b <*> (filesEqual f1 f2)) (return True) (zip fs1 fs2)
+  -- zip :: [a] -> [b] -> [(a, b)]
+  -- foldl :: (a -> b -> a) -> a -> [b] -> a
+--  do d1Contents <- contents fs1
+--     d2Contents <- contents fs2
+--     return $ sort fs1 == sort fs2 && d1Contents == d2Contents
+
+-- call filesEqual for all the files in the given directories
+dirsEqual :: FilePath -> FilePath -> IO Bool
+dirsEqual d1 d2 =
+  do d1Filenames <- filesWithFullPaths d1
+     d2Filenames <- filesWithFullPaths d2
+     fileListEqual d1Filenames d2Filenames
+
+-- read the contents of a file
+-- transform the contents
+-- write them back out to a new file
+transformFile :: FilePath -> (String -> String) -> FilePath -> IO ()
+transformFile inFile f outFile = f <$> (readFile inFile) >>= (writeFile outFile)
+
+{------------------------
+Be careful, as these ones are pretty dangerous, it seems.
+I was running into "too many open file errors".
+------------------------}
 
 -- contents for a list of files
 contents :: [FilePath] -> IO [String]
@@ -42,29 +85,3 @@ dirFileNamesAndContents dir = zip <$> (filesWithFullPaths dir) <*> (dirContents 
 -- filenames and contents of every file in the given list
 namesAndContents :: [FilePath] -> IO [(String, String)]
 namesAndContents fileNames = zip fileNames <$> contents fileNames
-
---sequenceA_ :: (Foldable t, Applicative f) => t (f a) -> f ()
-putFileNames :: FilePath -> IO ()
-putFileNames dir = filesWithFullPaths dir >>= putList 
-
--- checks to see if the contents of the given files are equal
-filesEqual :: FilePath -> FilePath -> IO Bool
-filesEqual f1 f2 = (==) <$> (readFile f1) <*> (readFile f2)
-
-fileListEqual :: [FilePath] -> [FilePath] -> IO Bool
-fileListEqual fs1 fs2 =
-  do d1Contents <- contents fs1
-     d2Contents <- contents fs2
-     return $ sort fs1 == sort fs2 && d1Contents == d2Contents
-
--- checks to see if the two given directories
---   1) contain all the same files
---   2) the contents of the files are equal
-dirsEqual :: FilePath -> FilePath -> IO Bool
-dirsEqual d1 d2 =
-  do d1Filenames <- filesWithFullPaths d1
-     d2Filenames <- filesWithFullPaths d2
-     fileListEqual d1Filenames d2Filenames
-
-readDoWrite :: FilePath -> (String -> String) -> FilePath -> IO ()
-readDoWrite inFile f outFile = f <$> (readFile inFile) >>= (writeFile outFile)
