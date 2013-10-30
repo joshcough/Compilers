6a6895d7de68fe98c2b438ac68fb52fba508add4
joshcough <joshcough@gmail.com>
2013-10-30 14:04:51 -0400

    removed old haskell src code

--- scala/src/main/haskell/Testhelpers.hs
+++
@@ -1,9 +0,0 @@
-module TestHelpers (makeTest, runTests) where
-
-import Test.HUnit
-
-makeTest :: (Eq a, Show a) => String -> a -> a -> Test
-makeTest name expected actual = TestLabel name (TestCase (assertEqual name expected actual))
-
-runTests ts = runTestTT (TestList (ts))
-
